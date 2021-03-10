cc8030d3879b65b6b20c203d7ef8bde9c56747ea
ZAHERSKIY <zahar.kirillov228@gmail.com>
2021-03-11 01:08:15 +0300

    Last version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n.toString(10);
+    let reverseValue = str.split("").reverse().join("");
+
+    return parseInt(reverseValue);
+};
